97ccbc51ae4b47feda94f6bf967e4b2fda0b3c72
xiuxiuu <hscheina@gmail.com>
2026-01-23 15:41:48 -0500

    Coding exercise 4

---
+++ Methods - AbsoluteOfSum method - Coding exercise/Methods - AbsoluteOfSum method - Coding exercise/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static int AbsoluteOfSum(int a, int b)
+        {
+            int sum = a + b;
+            return Math.Abs(sum);
+        }
+    }
+}
