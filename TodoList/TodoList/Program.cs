75822e58e7a4852e1d5e2cfeae7a8da7d9f007d6
xiuxiuu <hscheina@gmail.com>
2026-01-23 16:43:15 -0500

    Lecture 34
    int.Parse method

--- TodoList/TodoList/Program.cs
+++ TodoList/TodoList/Program.cs
@@ -1,17 +1,17 @@
 ﻿using System.ComponentModel.Design;
+Console.WriteLine("Provide a number.");
+string userInput = Console.ReadLine();
+int number = int.Parse(userInput);
+Console.WriteLine(number);
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine("[S]ee all TODOs]");
+//Console.WriteLine("[A]dd a TODO");
+//Console.WriteLine("[R]emove a TODO");
+//Console.WriteLine("[E]xit");
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("[S]ee all TODOs]");
-Console.WriteLine("[A]dd a TODO");
-Console.WriteLine("[R]emove a TODO");
-Console.WriteLine("[E]xit");
+////reads line from console and stores it in userInput variable
+//var userInput = Console.ReadLine();
 
-//reads line from console and stores it in userInput variable
-var userInput = Console.ReadLine();
-bool isLong = IsLong(userInput);
-
-var result = Add(5, 10);
-Console.WriteLine("10 + 5 = " + result);
 //if (userInput == "S" || userInput == "s")
 //{
 //    PrintSelectedOption("See all TODOs");
@@ -33,28 +33,3 @@ Console.ReadKey(); //preventing window from closing immediately
 //{
 //    Console.WriteLine("Selected option: " + selectedOption);
 //}
-
-int Add(int a, int b)
-{
-    //if (a > 10)
-    //{
-    //    return a + b; // error - not all code paths return a value
-    //}
-    return a + b;
-}
-
-bool IsLong(string input)
-{
-    //if(input.Length > 10)
-    //{
-    //    return true;
-    //}
-    ////else
-    ////{
-    ////    return false;
-    ////}
-    //return false;
-
-    return input.Length > 10;
-    //code refactoring - the process of improving the code quality without altering its behavior
-}
--- TodoList/TodoList/obj/Debug/net10.0/TodoList.AssemblyInfo.cs
+++ TodoList/TodoList/obj/Debug/net10.0/TodoList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bac476c615905536fd1206b1d81d1ba134b1658")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97ccbc51ae4b47feda94f6bf967e4b2fda0b3c72")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
