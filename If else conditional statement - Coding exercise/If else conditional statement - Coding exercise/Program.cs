a66ae6832ade45add30dd31329e121ed2d451655
xiuxiuu <hscheina@gmail.com>
2026-01-23 13:38:04 -0500

    Coding exercise 3

---
+++ If else conditional statement - Coding exercise/If else conditional statement - Coding exercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static string IsElseConditionalStatement()
+        {
+            int number = 0;
+
+            string result;
+
+            if (number < 0)
+            {
+                result = "negative";
+            }
+            else if (number == 0)
+            {
+                result = "zero";
+            }
+            else
+            {
+                result = "positive";
+            }
+            return result;
+        }
+    }
+}
