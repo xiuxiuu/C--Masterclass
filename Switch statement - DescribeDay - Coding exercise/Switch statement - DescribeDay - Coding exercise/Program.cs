98c8677971a0bcecbece646c7883489d2e0266e2
xiuxiuu <hscheina@gmail.com>
2026-01-25 14:43:42 -0500

    Coding exercise 6

---
+++ Switch statement - DescribeDay - Coding exercise/Switch statement - DescribeDay - Coding exercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static string DescribeDay(int dayNumber)
+        {
+            switch (dayNumber)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                    return "Working day";
+                case 6:
+                case 7:
+                    return "Weekend";
+                default:
+                    return "Invalid day number";
+            }
+        }
+    }
+}
