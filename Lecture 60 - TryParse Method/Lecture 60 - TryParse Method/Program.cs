4497d31ad3804a66208c80690be60440b538ad58
xiuxiuu <hscheina@gmail.com>
2026-01-28 12:12:41 -0500

    Lecture 60 - TryParse method

---
+++ Lecture 60 - TryParse Method/Lecture 60 - TryParse Method/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Intrinsics.Arm;
+
+//Console.WriteLine("Enter a number: ");
+//var userInput = Console.ReadLine();
+bool isParsingSuccessful;
+do
+{
+    Console.WriteLine("Enter a number: ");
+    var userInput = Console.ReadLine();
+
+    isParsingSuccessful = int.TryParse(userInput, out int number);
+
+    if (isParsingSuccessful)
+    {
+        Console.WriteLine($"Parsing worked, number is: {number}");
+    }
+    else
+    {
+        Console.WriteLine("Parsing was not successful.");
+    }
+} while (!isParsingSuccessful);
+
+//if (isParsingSuccessful)
+//{
+//    Console.WriteLine($"Parsing worked, number is: {number}");
+//}
+//else
+//{
+//    Console.WriteLine("Parsing was not successful.");
+//}
+
+    Console.ReadKey();
