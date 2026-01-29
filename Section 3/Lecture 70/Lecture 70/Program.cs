3deb077d4d3211ffb21ccd6818efd6348d981b69
xiuxiuu <hscheina@gmail.com>
2026-01-29 14:13:52 -0500

    Abstraction

---
+++ Section 3/Lecture 70/Lecture 70/Program.cs
@@ -0,0 +1,18 @@
+﻿var internationalPizzaDay23 = new DateTime(2023, 2, 9);
+
+Console.WriteLine($"Year is: {internationalPizzaDay23.Year}");
+Console.WriteLine($"Month is: {internationalPizzaDay23.Month}");
+Console.WriteLine($"Day is: {internationalPizzaDay23.Day}");
+Console.WriteLine($"Day of the week is: {internationalPizzaDay23.DayOfWeek}");
+
+Console.WriteLine();
+
+var internationalPizzaDay24 = internationalPizzaDay23.AddYears(1);
+var internalData = internationalPizzaDay24.dateData; //only give the users the interface to operate on the type, hide implementation details
+
+Console.WriteLine($"Year is: {internationalPizzaDay24.Year}");
+Console.WriteLine($"Month is: {internationalPizzaDay24.Month}");
+Console.WriteLine($"Day is: {internationalPizzaDay24.Day}");
+Console.WriteLine($"Day of the week is: {internationalPizzaDay24.DayOfWeek}");
+
+Console.ReadKey();
