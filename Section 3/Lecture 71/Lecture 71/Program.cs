4cf860b549f16fa6f3f98c10809d940420baf3d8
xiuxiuu <hscheina@gmail.com>
2026-01-29 16:27:00 -0500

    Our first class

---
+++ Section 3/Lecture 71/Lecture 71/Program.cs
@@ -0,0 +1,12 @@
+﻿//var rectangle1 = new Rectangle(5, 10); //no constructor
+var rectangle1 = new Rectangle();
+Console.WriteLine($"Width is: {rectangle1.width}");
+Console.WriteLine($"Height is: {rectangle1.height}");
+
+Console.ReadKey();
+
+class Rectangle
+{
+    int width;
+    int height;
+}
