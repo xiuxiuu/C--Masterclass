9b18ad1f842fbaa5148dca7235908a8527bafcd1
xiuxiuu <hscheina@gmail.com>
2026-02-09 12:39:02 -0500

    Dog class - coding exercise 17

---
+++ Section 3/Dog class - coding exercise 17/Dog class - coding exercise 17/Program.cs
@@ -0,0 +1,66 @@
+﻿/* Dog class
+
+Define a Dog class. Each Dog will have a name(string), breed (string), and weight (int) fields.
+
+This class will have two constructors:
+
+    The first takes name, breed, and weight parameters (in this order).
+
+    The second takes only the name and weight parameters (in this order). In this case, the breed should be assigned the "mixed-breed" value.
+
+This class will expose a single public method called Describe, returning a string describing the dog.
+
+    For a dog named Lucky, with the breed "german shepherd" and weight of 40, this method should return "This dog is named Lucky, it's a german shepherd, and it weighs 40 kilograms, so it's a large dog."
+
+    For a dog named Tina, with the breed "shar pei" and weight of 25, this method should return "This dog is named Tina, it's a shar pei, and it weighs 25 kilograms, so it's a medium dog."
+
+The rule for translating the dog's weight to the weight description is as follows:
+
+    For dogs lighter than 5 kilos, it is "tiny".
+
+    For dogs weighing 5 kilos or more but less than 30 kilos, it is "medium".
+
+    For other dogs, it is "large".*/
+using System;
+
+namespace Coding.Exercise
+{
+    public class Dog
+    {
+        string _name;
+        string _breed;
+        int _weight;
+
+        public Dog(string name, string breed, int weight)
+        {
+            _name = name;
+            _breed = breed;
+            _weight = weight;
+        }
+
+        public Dog(string name, int weight, string breed = "mixed-breed")
+        {
+            _name = name;
+            _breed = breed;
+            _weight = weight;
+        }
+
+        public string Describe()
+        {
+            string weightDescription;
+            if (this._weight < 5)
+            {
+                weightDescription = "tiny";
+            }
+            else if (this._weight > 5 && this._weight < 30)
+            {
+                weightDescription = "medium";
+            }
+            else
+            {
+                weightDescription = "large";
+            }
+            return ($"This dog is named {this._name}, it's a {this._breed}, and it weighs {this._weight} kilograms, so it's a {weightDescription} dog.");
+        }
+    }
+}
