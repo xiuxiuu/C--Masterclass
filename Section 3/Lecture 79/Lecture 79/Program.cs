838913f6d3ee8366ab94cf5bfd2e2c25fe73ef76
xiuxiuu <hscheina@gmail.com>
2026-02-10 13:05:49 -0500

    Lecture 87 - Computed properties

--- Section 3/Lecture 79/Lecture 79/Program.cs
+++ Section 3/Lecture 79/Lecture 79/Program.cs
@@ -1,6 +1,5 @@
 ﻿var rectangle1 = new Rectangle(5, 10);
 Console.WriteLine($"Width is: {rectangle1.Width}");
-//rectangle1.Width = 15;
 Console.WriteLine($"Height is: {rectangle1.GetHeight()}");
 Console.WriteLine($"Area is: {rectangle1.CalculateArea()}");
 Console.WriteLine($"Circumference is: {rectangle1.CalculateCircumference}");
@@ -21,25 +20,9 @@ class Rectangle
         _height = GetLengthOrDefault(height, nameof(_height));
     }
 
-    //backing field
-    //private int _width;
     private int _height;
 
     public int Width { get; private set; }
-    //public int Width
-    //{
-    //    get
-    //    {
-    //        return _width;
-    //    }
-    //    private set
-    //    {
-    //        if (value > 10)
-    //        {
-    //            _width = value;
-    //        }
-    //    }
-    //}
     public int GetHeight() => _height;
 
     public void SetHeight(int value)
@@ -61,16 +44,7 @@ class Rectangle
     }
     public int CalculateArea() => Width * _height;
     public int CalculateCircumference() => 2 * (Width + _height);
+
+    //computed properties, should never be performance-heavy
+    public string Description => $"A rectangle with width {Width} and height {_height}";
 }
-
-
-//fields: variable-like
-    //single access modifier
-    //no separate getter and setter
-    //cannot be overridden in derived classes
-    //should always be private
-//properties: method-like
-    //separate access modifiers for getter and setter
-    //getter or setter may be removed
-    //can be overridden in derived classes
-    //can safely be public
